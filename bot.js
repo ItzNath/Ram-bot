2d9b467ff38147674618cbc54eef87166d7ed813
ItzNath <nathan44game@gmail.com>
2017-12-13 22:23:45 -0500

    Correcting few things

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const objBot = new Discord.Client();
 const settings = require ('./settings.json');
+const prefix = "!"
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -13,4 +14,4 @@ client.on('message', msg => {
   };
 });
 
-client.login(settings.token);
+client.login("tal2PBYFR_j_m7FWiyzb9JDwanyYVSwC");
